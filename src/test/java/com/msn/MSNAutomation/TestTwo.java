fa302a2f657db92562e5fa1b9fd170a55ff1f990
pramaraju96 <89196905+pramaraju96@users.noreply.github.com>
2022-02-22 22:19:36 +0530

    Create TestTwo.java

---
+++ src/test/java/com/msn/MSNAutomation/TestTwo.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestTwo {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test1() {
+		System.out.println("Inside test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Inside test2");
+	}
+	@Test
+	public void test3() {
+		System.out.println("Inside test3");
+	}
+
+}
